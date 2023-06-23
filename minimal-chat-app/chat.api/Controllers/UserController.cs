dd2100c3cdd2d9b32f948b5ec25c5f84c85fddfd
Akshay Devarde <akshay@promactinfo.com>
2023-06-23 10:46:23 +0530

    #Updated changes with Async Task

--- minimal-chat-app/chat.api/Controllers/UserController.cs
+++ minimal-chat-app/chat.api/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Chat.DominModel.DTOs;
+﻿
+using Chat.DominModel.DTOs;
 using Chat.DominModel.Model;
 using Chat.Repository;
 using Microsoft.AspNetCore.Http;
@@ -18,9 +19,9 @@ namespace Minimal_chat_application.Controllers
         }
 
         [HttpGet("{id}")]
-        public IActionResult Get(int id)
-        {
-            var user = _userRepository.Get(id);
+        public async Task<IActionResult> Get(int id)
+        {  
+            var user =await _userRepository.Get(id);
             if (user == null)
             {
                 return NotFound();
@@ -37,9 +38,9 @@ namespace Minimal_chat_application.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            var users = _userRepository.GetAll();
+            var users =await _userRepository.GetAll();
             var response = users.Select(user => new UserResponse
             {
                 UserId = user.UserId,
@@ -52,7 +53,7 @@ namespace Minimal_chat_application.Controllers
 
         [HttpPost]
         [Route("/api/register")]
-        public IActionResult Insert([FromBody] User user)
+        public async Task<IActionResult> Insert([FromBody] User user)
         {
             if (!ModelState.IsValid)
             {
@@ -63,7 +64,7 @@ namespace Minimal_chat_application.Controllers
                 return BadRequest(errorResponse);
             }
 
-            var existingUser = _userRepository.GetByEmail(user.Email);
+            var existingUser =await  _userRepository.GetByEmail(user.Email);
             if (existingUser != null)
             {
                 var errorResponse = new ErrorResponse
@@ -73,7 +74,7 @@ namespace Minimal_chat_application.Controllers
                 return Conflict(errorResponse);
             }
 
-            _userRepository.Insert(user);
+            await _userRepository.Insert(user);
 
             var response = new UserResponse
             {
@@ -86,47 +87,17 @@ namespace Minimal_chat_application.Controllers
             return Ok(response);
         }
 
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, [FromBody] User user)
-        {
-            if (!ModelState.IsValid)
-            {
-                var errorResponse = new ErrorResponse
-                {
-                    Error = "Invalid user data."
-                };
-                return BadRequest(errorResponse);
-            }
-
-            var existingUser = _userRepository.Get(id);
-            if (existingUser == null)
-            {
-                return NotFound();
-            }
-
-            user.UserId = id;
-            _userRepository.Update(user);
-
-            var response = new UserResponse
-            {
-                UserId = user.UserId,
-                Name = user.Name,
-                Email = user.Email,
-            };
-
-            return Ok(response);
-        }
-
+       
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            var existingUser = _userRepository.Get(id);
+            var existingUser =await _userRepository.Get(id);
             if (existingUser == null)
             {
                 return NotFound();
             }
 
-            _userRepository.Delete(id);
+          await  _userRepository.Delete(id);
 
             var response = new UserResponse
             {
